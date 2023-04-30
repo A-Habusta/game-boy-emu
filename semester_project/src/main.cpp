cdb42b53351e96f0a4aaf5a039f9f7d1bf578239
Adrian Habusta <119956160+A-Habusta@users.noreply.github.com>
2023-04-30 02:00:18 +0200

    Change constant rom paths to passing them from the command-line

--- semester_project/src/main.cpp
+++ semester_project/src/main.cpp
@@ -6,11 +6,25 @@
 
 #include "emulator.hpp"
 
-using namespace std::literals::string_view_literals;
-
 constexpr int screen_size_factor = 4;
 
-int main() {
+void free_sdl(SDL_Renderer* renderer, SDL_Window* window) {
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
+int main(int argc, char** argv) {
+    if(argc < 3) {
+        std::cout << "Not enough arguments! Expected 2 or 3." << std::endl;
+        return 1;
+    }
+
+    if(argc > 4) {
+        std::cout << "Too many arguments! Expected 2 or 3." << std::endl;
+        return 1;
+    }
+
     int window_height = pixel_processing_unit::screen_pixel_height * screen_size_factor;
     int window_width = pixel_processing_unit::screen_pixel_width * screen_size_factor;
 
@@ -24,25 +38,29 @@ int main() {
                                               SDL_WINDOW_OPENGL);
     SDL_Renderer* renderer = SDL_CreateRenderer(main_window, -1, SDL_RENDERER_ACCELERATED);
 
-    std::string_view boot_rom_path = "/home/adrian/Downloads/dmg_boot.bin"sv;
-    std::string_view rom_path = "/home/adrian/Downloads/dr_mario.gb"sv;
-    //std::string_view rom_path = "/home/adrian/Downloads/tetris.gb"sv;
-    std::string_view sram_path = ""sv;
+    std::string_view boot_rom_path = argv[1];
+    std::string_view rom_path = argv[2];
+    std::string_view sram_path = argc == 4 ? argv[3] : "";
 
-    emulator::emulator emu(renderer, boot_rom_path, rom_path, sram_path);
+    std::optional<emulator::emulator> emu{std::nullopt};
+    try {
+        emu.emplace(renderer, boot_rom_path, rom_path, sram_path);
+    }
+    catch (const std::runtime_error& e) {
+        std::cout << e.what() << std::endl;
+        free_sdl(renderer, main_window);
+        return 1;
+    }
 
     SDL_RenderClear(renderer);
     SDL_RenderPresent(renderer);
 
     while(true) {
         try {
-            emu.execute_cpu();
+            emu->execute_cpu();
         }
         catch (const emulator::exit&) {
-            SDL_DestroyRenderer(renderer);
-            SDL_DestroyWindow(main_window);
-            SDL_Quit();
-
+            free_sdl(renderer, main_window);
             return 0;
         }
     }
